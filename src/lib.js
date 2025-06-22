bef8cc914c42d411464c53df18dd7f870dc1ba1d
Mixanik <xxmixanikxx@gmail.com>
2025-06-23 02:17:28 +0300

    added lib building using vite

--- src/lib.js
+++ src/lib.js
@@ -2,3 +2,4 @@ export { Node } from './components/node.js';
 export { Port } from './components/port.js';
 export { Connection, PreviewConnection } from './components/connection.js';
 export { Editor } from './components/editor.js';
+import './styles.css';
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib.js'),
+      name: 'yanbe',
+      fileName: 'yanbe',
+      cssFileName: 'yanbe'
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {},
+      },
+    },
+  },
+});
